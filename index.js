5d9d7b7673bf5b093df54819537762e9e0d65127
makerboomRijnland <makerboom@mborijnland.nl>
2023-06-01 07:34:34 +0200

    Showing monsters

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 let monsters = [];
+let currentPage = 1;
 
 function setup() {
     fetch('https://dl.dropboxusercontent.com/s/iwz112i0bxp2n4a/5e-SRD-Monsters.json')
         .then((response) => response.json())
         .then(parseMonsters);
 
+    document.querySelector('#monster-nav .prev').addEventListener('click', prevMonsters);
+    document.querySelector('#monster-nav .next').addEventListener('click', nextMonsters);
+}
+
+function nextMonsters() {
+    const page = currentPage * 10 >= monsters.length ? 1 : currentPage + 1;
+    showMonsters(page);
+}
+
+function prevMonsters() {
+    const page = currentPage == 1 ? Math.ceil(monsters.length / 10) : currentPage - 1;
+    showMonsters(page);
 }
 
 function parseMonsters(data) {
+    data.pop();
     monsters = data;
 
-    showMonster(monsters[0]);
+    showMonsters();
 }
 
-function showMonsters() {
-    document.body.innerHTML = "";
+function showMonsters(page = 1, count = 10) {
+    const firstIndex = (page - 1) * count;
+    const lastIndex = firstIndex + count;
+    const container = document.getElementById('monsters');
+    currentPage = page;
+    
+    container.innerHTML = "";
 
-    for(let monster of monsters) {
-        showMonster(monster);
+    for(let index = firstIndex; index < lastIndex; index++) {
+        if(monsters[index]) {
+            addMonster(monsters[index]);
+        }
     }
 }
 
-function showMonster(monster) {
+function addMonster(monster) {
+    const container = document.getElementById('monsters');
     const monsterTemplate = document.getElementById('monster-template');
     const monsterArticle = monsterTemplate.content.cloneNode(true);
 
@@ -31,7 +53,14 @@ function showMonster(monster) {
     const monsterDescription = monsterArticle.querySelector('p.description');
     monsterDescription.innerHTML = `${monster.size} ${monster.alignment} ${monster.type}`;
 
-    document.body.appendChild(monsterArticle);
+    const actionList = monsterArticle.querySelector('ul.actions');
+    for(let action of monster.actions) {
+        const actionItem = document.createElement('li');
+        actionItem.innerHTML = action.name;
+        actionList.appendChild(actionItem);
+    }
+
+    container.appendChild(monsterArticle);
 }
 
 window.addEventListener('load', setup);
